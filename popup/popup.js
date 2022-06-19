c63758706eb2ebbe30b6c357a3e95ae1ef5c1ecf
PrashanthaTP <tpprashantha@gmail.com>
2022-06-19 21:48:51 +0530

    Feat(Setup): Add manifest.json and popup related files

---
+++ popup/popup.js
@@ -0,0 +1,25 @@
+//https://docs.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/part2-content-scripts
+//https://betterprogramming.pub/the-ultimate-guide-to-building-a-chrome-extension-4c01834c63ec
+
+const form = document.getElementById("form-main");
+const inputName = document.getElementById("input-name");
+//callback to send message to content script
+const sendMessage = (tabs)=>{
+    chrome.tabs.sendMessage(tabs[0].id,
+                            {action:"RENAME_TAB",
+                            props:{name:inputName.value}
+                            });
+};
+
+form.onsubmit=(e)=>{
+    e.preventDefault();
+    chrome.tabs.query({active:true,currentWindow:true},sendMessage);
+    window.close();
+};
+
+/*
+ * https://developer.chrome.com/docs/extensions/reference/commands/
+ */
+window.addEventListener('DOMContentLoaded', (event) => {
+    inputName.focus();
+});
